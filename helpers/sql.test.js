5e88f9de29b1eca6f8ae3646839fba54d9a5b45b
Le Pham <phamtle724@gmail.com>
2022-08-24 12:27:46 -0700

    wrote two tests for sqlForPartialUpdate

---
+++ helpers/sql.test.js
@@ -0,0 +1,40 @@
+"use strict";
+const { sqlForPartialUpdate } = require("./sql");
+
+const { BadRequestError } = require("../expressError");
+
+
+describe("sqlForPartialUpdate", function () {
+  test("works when there's data", function () {
+    const dataToUpdate = {
+      firstName: "jon",
+      lastName: "snow",
+      email: "jonsnow@got.com"
+    };
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+    const update = sqlForPartialUpdate(dataToUpdate, jsToSql);
+    expect(update).toEqual({
+      setCols: '"first_name"=$1, "last_name"=$2, "email"=$3',
+      values: ["jon", "snow", "jonsnow@got.com"]
+    });
+  });
+
+  test("does not work when there's no data", function () {
+    const dataToUpdate = {};
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+    try {
+      sqlForPartialUpdate(dataToUpdate, jsToSql);
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  })
+
+});
